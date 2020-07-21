b41670b22083d7948eb23e4fd8cd6516acb81b2c
Gunnar Helgason <gh@vitasim.dk>
2020-07-21 07:48:12 +0200

    message

---
+++ src/components/AppContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default class AppContainer extends React.Component {
+  render() {
+    const { children } = this.props;
+    return <div className="app-container">{children}</div>;
+  }
+}
